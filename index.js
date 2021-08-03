3448e17b0bd890171510c7873fe639778b3b3ed7
Levi M. Williamson <levimwill@gmail.com>
2021-08-03 15:36:29 -0700

    Added random sensor data points.

--- index.js
+++ index.js
@@ -8,3 +8,35 @@ const tileLayerOptions = {
 	accessToken: 'pk.eyJ1IjoibGV2aW13aWxsaWFtc29uIiwiYSI6ImNrcnY0Y3N5MzAzMHkyb24wNW9mMnhsN3EifQ.JnuBDAaykgIF24NJmdbi6A'
 }
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', tileLayerOptions).addTo(leafletMap)
+
+function generateRandomSensorDataList(centre, dimensions, intensityRange, count) {
+	const [centreX, centreY] = centre
+	const [width, height] = dimensions
+	const [intensityStart, intensityEnd] = intensityRange
+
+	const sensorDataList = []
+	for(let i = 0; i < count; ++i)
+		sensorDataList.push({
+			position: [
+				centreX - width / 2 + Math.random() * width,
+				centreY - height / 2 + Math.random() * height
+			],
+			intensity: intensityStart + Math.random() * (intensityEnd - intensityStart)
+		})
+	return sensorDataList
+}
+
+const sensorDataList = generateRandomSensorDataList(
+	[48.8847, -124.3661],
+	[0.1, 0.1],
+	[0, 10],
+	33
+)
+for(let index in sensorDataList) {
+	const { position, intensity } = sensorDataList[index]
+	L.marker(position)
+		.addTo(leafletMap)
+		.bindPopup(`<b>Sensor ${index}</b>: <i>${intensity}</i>`)
+		.openPopup()
+	console.log(`Added marker:\n\t[${position[0]}, ${position[1]}]\n\t${intensity}`)
+}
